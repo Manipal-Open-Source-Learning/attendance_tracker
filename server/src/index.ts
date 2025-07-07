652ad2a80ef138fe74acd90cc6815988f798c5be
PranavU-Coder <pranavu8406@gmail.com>
2025-07-07 22:11:28 +0530

    refactored frontend code to /client folder and created /server folder for backend code with basic node + express implementation and use of mongoDB Atlas

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// MONGO DB CONNECTION
+
+mongoose.connect(process.env.MONGODB_URI as string)
+  .then(() => console.log('MongoDB Atlas connected successfully'))
+  .catch(err => console.error('Database connection error:', err));
+
+// MIDDLEWARE WE GONNA ADD 
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ROUTERS
+
+app.get('/', (_req: Request, res: Response) => {
+  res.json({ message: 'WE COOKING BABY!!!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+// WE MIGHT ADD/REMOVE ANY OF THE FOLDERS DEPENDING ON HOW THIS TURNS UP
