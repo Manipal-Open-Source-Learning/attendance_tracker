9d97a2d3371575b927cbd4bf909312ee82ca1dd7
ObnoxiousOrca <darkdragonlord30805@gmail.com>
2025-07-08 19:24:34 +0530

    checkpoint: Calendar completion

--- src/components/ui/Calendar.tsx
+++ src/components/ui/Calendar.tsx
@@ -1,6 +1,9 @@
-const Days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
-const Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-const extraDays = [3,2,3,2,3,2,3]
+import useDateStore from "@/stores/useDateStore";
+
+const Days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+const Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const extraDays = [3,2,3,2,3,2,3];
+const TODAY = new Date();
 
 function checkLeap(n: number){
     if(n%4==0){
@@ -10,36 +13,49 @@ function checkLeap(n: number){
     return 0;
 }
 
+function isToday(activeDate: Date, date: number){
+    if(date==TODAY.getDate() && activeDate.getMonth()==TODAY.getMonth() && activeDate.getFullYear()==TODAY.getFullYear()){
+        return true;
+    }
+    return false;
+}
+
 const Calendar = ()=>{
-    var currDate = new Date();
-    var activeDate = currDate;
+    const {activeDate, nextMonth, prevMonth, setDate} = useDateStore();
     return (
         <>
             <div className="max-w-120">
+
                 <div className="flex justify-between bg-amber-600 w-350px p-1 items-center rounded-t-xl">
-                    <button id="previous" className="cursor-pointer bg-[#ffb380] text-2xl text-white h-7 w-7 flex justify-center items-center rounded-lg">‹</button>
-                    <span>{Months[currDate.getMonth()]}, {currDate.getFullYear()}</span>
-                    <button id="next" className="cursor-pointer bg-[#ffb380] text-2xl text-white h-7 w-7 flex justify-center items-center rounded-lg">›</button>
+                    <button id="previous" className="cursor-pointer bg-[#ffb380] text-2xl text-white h-7 w-7 flex justify-center items-center rounded-lg"
+                        onClick={prevMonth}>‹</button>
+                    <span>{Months[activeDate.getMonth()]}, {activeDate.getFullYear()}</span>
+                    <button id="next" className="cursor-pointer bg-[#ffb380] text-2xl text-white h-7 w-7 flex justify-center items-center rounded-lg"
+                        onClick={nextMonth}>›</button>
                 </div>
+
+
                 <div className="flex justify-between bg-[#c4c4c4] pl-1 pr-1">
                     {Days.map((e)=>(
                         <p className="w-[calc(100%/7)] text-center">{e}</p>
                     ))}
                 </div>
-                <div className="flex flex-wrap bg-[#ffb380] p-1 rounded-b-xl">
-                    {[...Array((7-currDate.getDate()%7)%7 - currDate.getDay()+1)].map(()=>(
+
+
+                <div id="dates" className="flex flex-wrap bg-[#ffb380] p-1 pb-2 rounded-b-xl">
+                    {[...Array((new Date(activeDate.getFullYear(), activeDate.getMonth(),1)).getDay())].map(()=>(
                             <p className="h-10 w-[calc(100%/7)] border-0"></p>)
                         )
                     }
-                    {[...Array(28+((currDate.getMonth()==1)?checkLeap(currDate.getFullYear()):extraDays[currDate.getMonth()%6])).keys()].map((e)=>(
-                            <button className="h-10 w-[calc(100%/7)] border-0 cursor-pointer flex justify-center items-center">
-                                <p className={`w-8 h-8 rounded-full flex justify-center items-center ${currDate.getDate()==e+1?"bg-green-500":""}`}>
+                    {[...Array(28+((activeDate.getMonth()==1)?checkLeap(activeDate.getFullYear()):extraDays[activeDate.getMonth()%7])).keys()].map((e)=>(
+                            <button className="h-15 w-[calc(100%/7)] border-0 cursor-pointer flex justify-center items-center"
+                                onClick={()=>setDate(new Date(activeDate.getFullYear(), activeDate.getMonth(), e+1))}>
+                                <p className={`w-8 h-8 rounded-full flex justify-center items-center border-amber-600 ${isToday(activeDate, e+1)?"bg-green-500":""} border-${(activeDate.getDate()==e+1)?"3":"0"}`}>
                                     {e+1}
                                 </p>
                             </button>)
                         )
                     }
-
                 </div>
                 <br/>
                 <br/>
