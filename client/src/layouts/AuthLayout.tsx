752f267c81955bb01cb9fc52d62ca6b4fb871b01
Firez <55920870+FirezTheGreat@users.noreply.github.com>
2025-07-08 12:29:42 +0530

    Update AuthLayout.tsx

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -1,7 +1,5 @@
 import { Outlet } from 'react-router-dom';
 
-// REMOVING BG-PRIMARY
-
 const AuthLayout = () => {
   return (
     <div className="relative min-w-full bg-[linear-gradient(to_bottom,hsl(210,6%,6%)_0%,hsl(0,0%,3%)_50%)]">
